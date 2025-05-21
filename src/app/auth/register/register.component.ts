101dd6a87ef094512589d2d08564fff485c1fade
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-05-21 19:09:50 +0200

    feat: add register component with form handling and routing

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm = {
+    email: '',
+    username: '',
+    password: '',
+    confirmPassword: ''
+  }
 
+  onSubmit() {
+    console.log("Register submitted", this.registerForm);
+    // real logic goes here
+  }
 }
