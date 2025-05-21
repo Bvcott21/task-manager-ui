b661fe15b425235b235fa98469106d2f5ca04d3a
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-05-21 19:21:06 +0200

    feat: update header component with navigation links and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,6 @@ import { RegisterComponent } from './auth/register/register.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: '' }
 ];
