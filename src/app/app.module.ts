1f6487349bb4fe7bfbf7ecc004b052f7892dbe07
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-05-21 18:42:39 +0200

    feat: add login and register components with basic structure and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // required for Angular Material
 import { RouterModule } from "@angular/router";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     declarations: [
         AppComponent,
+        HeaderComponent
     ],
     imports:[
         BrowserModule,
+        BrowserAnimationsModule,
         AppRoutingModule, 
-        RouterModule
+        RouterModule,
+        MatToolbarModule,
+        HeaderComponent
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err=> console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule)
+  ]
+}).catch(err => console.error(err));
